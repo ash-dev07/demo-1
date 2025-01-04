4666363921df932a616dd3eff83efff00599ebd3
ash-dev07 <ashishs468@gmail.com>
2025-01-04 14:14:26 +0530

    Update A.java

--- A.java
+++ A.java
@@ -3,4 +3,6 @@ class A {
     public static void main(String[] args) {
         System.out.println("ashish");
         System.out.println("jhjhj");
+        System.out.println("jhjhj111");
+        
 }
